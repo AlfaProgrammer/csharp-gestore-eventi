02083438561cf8bcd848b885ed2a2b281420c43f
AlfaProgrammer <alexloghin@outlook.it>
2022-07-06 18:02:04 +0200

    Progressione sulla Milestone 1

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    class Evento
+    {
+        private string _titolo;
+        private DateTime _date;
+        public string Titolo
+        {
+            get
+            {
+                return this._titolo;
+            }
+
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    this._titolo = value;
+                }
+                else
+                {
+                    throw new Exception("Il titolo non può essere Nullo o Vuoto");
+                }
+            }
+        }
+        public DateTime Data { 
+            get
+            {
+                return this._date;
+            }
+            set
+            {
+                var currentDate = DateTime.Now;
+                var comparison = value.CompareTo(currentDate);
+                if( comparison == -1)
+                {
+                    throw new Exception("Hai impostato la data nel passato");
+                }
+                else
+                {
+                    this._date = value;
+                }
+            }       
+        }
+
+        public uint Capienza { get; }
+        public int PostiPrenotati { get;}
+
+        public Evento(string titolo, DateTime data, uint capienza )
+        {
+            this.Titolo = titolo;
+            this.Data = data;
+            this.Capienza = capienza;
+            this.PostiPrenotati = 0;
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using GestoreEventi;
+
+try
+{
+    var evento = new Evento("MioEvento", new DateTime(2012, 5, 12), 4512);
+}
+catch (Exception err)
+{
+
+    Console.WriteLine(err.Message);
+}
